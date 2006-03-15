a733dc2c601b642d2fe159a442fa86e3c2704252
gregoryyy <unknown>
2006-03-15 14:22:04 +0000

    *** empty log message ***

--- knowceans-tools/src/org/knowceans/util/TimeIntervals.java
+++ knowceans-tools/src/org/knowceans/util/TimeIntervals.java
@@ -11,6 +11,8 @@ import java.util.Date;
  * surrounding a given date. Intervals are expressed as 2-arrays of Date object,
  * corresponding to the start and end times. By convention, intervals include
  * their bounds.
+ * <p>
+ * FIXME: daylight saving time shift can create gaps in week and month intervals
  * 
  * @author gregor
  */
@@ -18,8 +20,17 @@ public class TimeIntervals {
 
     public static void main(String[] args) {
         Date now = new Date();
+        Date[] interval;
+
+        // today
+        interval = TimeIntervals.dayOf(now);
+        System.out.println("today:  \t" + interval[0] + " -- " + interval[1]);
+        // tomorrow
+        interval = TimeIntervals.daysAway(now, 1);
+        System.out.println("tomorrow:\t" + interval[0] + " -- " + interval[1]);
+
         // this week
-        Date[] interval = TimeIntervals.weekOf(now);
+        interval = TimeIntervals.weekOf(now);
         System.out.println("this week:\t" + interval[0] + " -- " + interval[1]);
         // next week
         interval = TimeIntervals.weeksAway(now, 1);
@@ -39,12 +50,29 @@ public class TimeIntervals {
         System.out.println("this year:\t" + interval[0] + " -- " + interval[1]);
         // next year
         Date[] interval2 = TimeIntervals.yearsAway(now, 1);
-        System.out.println("next year:\t" + interval[0] + " -- " + interval[1]);
-        
+        System.out.println("next year:\t" + interval2[0] + " -- "
+            + interval2[1]);
+
         interval = TimeIntervals.join(interval, interval2);
         // this and next year
-        System.out.println("this+next year:\t" + interval[0] + " -- " + interval[1]);
+        System.out.println("this+next year:\t" + interval[0] + " -- "
+            + interval[1]);
 
+        // exactly two weeks ago from today
+        interval = TimeIntervals.awayExactDay(now, Calendar.YEAR, -2);
+        System.out.println("ex. -2 months:\t" + interval[0] + " -- "
+            + interval[1]);
+
+    }
+
+    /**
+     * returns the start and end times of the day surrounding the date d.
+     * 
+     * @param d
+     * @return
+     */
+    public static Date[] dayOf(Date d) {
+        return daysAway(d, 0);
     }
 
     /**
@@ -55,7 +83,6 @@ public class TimeIntervals {
      */
     public static Date[] weekOf(Date d) {
         return weeksAway(d, 0);
-
     }
 
     /**
@@ -76,6 +103,28 @@ public class TimeIntervals {
      */
     public static Date[] yearOf(Date d) {
         return yearsAway(d, 0);
+    }
+
+    /**
+     * returns the start and end times of the day before / after the day given
+     * in date d.
+     * 
+     * @param d
+     * @param away
+     * @return
+     */
+    public static Date[] daysAway(Date d, int away) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(d);
+        Calendar start = Calendar.getInstance();
+        start.clear();
+        start.setLenient(true);
+        start.set(Calendar.YEAR, c.get(Calendar.YEAR));
+        start.set(Calendar.DAY_OF_YEAR, c.get(Calendar.DAY_OF_YEAR) + away);
+        Calendar end = (Calendar) start.clone();
+        end.add(Calendar.DAY_OF_YEAR, 1);
+        end.add(Calendar.MILLISECOND, -1);
+        return new Date[] {start.getTime(), end.getTime()};
     }
 
     /**
@@ -101,8 +150,8 @@ public class TimeIntervals {
     }
 
     /**
-     * returns the start and end times of the week before / after the start of
-     * the week surrounding the date d.
+     * returns the start and end times of the months before / after the start of
+     * the month surrounding the date d.
      * 
      * @param d
      * @param away
@@ -124,8 +173,8 @@ public class TimeIntervals {
     }
 
     /**
-     * returns the start and end times of the week before / after the start of
-     * the week surrounding the date d.
+     * returns the start and end times of the years before / after the start of
+     * the year surrounding the date d.
      * 
      * @param d
      * @param away
@@ -144,6 +193,83 @@ public class TimeIntervals {
         return new Date[] {start.getTime(), end.getTime()};
 
     }
+
+    /**
+     * returns the start and end times of the field (Calendar.*) before / after
+     * the start of the day surrounding the date d.
+     * 
+     * @param d the date to start from
+     * @param whatAway what time measure away (week, day, etc.)
+     * @param howManyAway how many of the time measure away
+     * @param howManyLong how many time measures as interval
+     * @return
+     */
+    public static Date[] awayExactDay(Date d, int whatAway, int howManyAway,
+        int howManyLong) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(d);
+        Calendar start = Calendar.getInstance();
+        start.clear();
+        start.setLenient(true);
+        start.setTime(d);
+        start.add(whatAway, howManyAway);
+        start.set(Calendar.HOUR_OF_DAY, 0);
+        start.set(Calendar.MINUTE, 0);
+        start.set(Calendar.SECOND, 0);
+        start.set(Calendar.MILLISECOND, 0);
+        Calendar end = (Calendar) start.clone();
+        end.add(whatAway, howManyLong);
+        end.add(Calendar.MILLISECOND, -1);
+        return new Date[] {start.getTime(), end.getTime()};
+    }
+
+    /**
+     * returns the start and end times of the field (Calendar.*) before / after
+     * the start of the day surrounding the date d, rounding the day. E.g.,
+     * awayExactDay(now, Calendar.WEEK_OF_YEAR, 8, 8) returns an 8 weeks
+     * interval from today 8 weeks ago to yesterday.
+     * 
+     * @param d the date to start from
+     * @param whatAway what time measure away (week, day, etc.)
+     * @param howManyAway how many of the time measure away
+     * @return
+     */
+    public static Date[] awayExactDay(Date d, int whatAway, int howManyAway) {
+        return awayExactDay(d, whatAway, howManyAway, 1);
+    }
+
+    // /**
+    // * returns the start and end times of the field (Calendar.*) before /
+    // after
+    // * the start of the field surrounding the date d, rounding the day. E.g.,
+    // * awayFromBeginOf(now, Calendar.WEEK_OF_YEAR, 8, 8) returns an 8 weeks
+    // * interval from Monday 8 weeks ago to last Sunday.
+    // *
+    // * @param d the date to start from
+    // * @param whatAway what time measure away (week, day, etc.)
+    // * @param howManyAway how many of the time measure away
+    // * @param howManyLong how many time measures as interval
+    // * @return
+    // */
+    // public static Date[] awayFromBeginOf(Date d, int whatAway, int
+    // howManyAway,
+    // int howManyLong) {
+    // Calendar c = Calendar.getInstance();
+    // c.setTime(d);
+    // Calendar start = Calendar.getInstance();
+    // start.clear();
+    // start.setLenient(true);
+    // start.setTime(d);
+    // start.add(whatAway, howManyAway);
+    // start.set(Calendar.HOUR_OF_DAY, 0);
+    // start.set(Calendar.MINUTE, 0);
+    // start.set(Calendar.SECOND, 0);
+    // start.set(Calendar.MILLISECOND, 0);
+    // Calendar end = (Calendar) start.clone();
+    // end.add(whatAway, howManyLong);
+    // end.add(Calendar.MILLISECOND, -1);
+    // return new Date[] {start.getTime(), end.getTime()};
+    // }
 
     /**
      * check whether time is within the interval.
